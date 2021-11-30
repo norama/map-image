4521f367bc9b7d47aa84608ea9ef9d4b3a6901f7
norama <norama.matema@gmail.com>
2021-11-30 12:12:36 +0100

    content: emotion

--- frontend/src/components/ItemContent.ts
+++ frontend/src/components/ItemContent.ts
@@ -9,19 +9,73 @@ export class ItemContent extends LitElement {
             margin: 10px 0;
         }
 
+        .emotion {
+            display: flex;
+        }
+
+        .radio {
+            display: flex;
+            align-items: center;
+            margin: 10px 60px 10px 0;
+        }
+
+        img {
+            width: 30px;
+            height: 30px;
+            cursor: pointer;
+        }
+
+        input[type='radio'] {
+            cursor: pointer;
+            transform: scale(1.5);
+            margin: 5px 20px 5px 0;
+        }
+
         textarea {
             padding: 8px;
         }
     `;
 
     commentRef = createRef<HTMLInputElement>();
+    emotionRef = createRef<HTMLInputElement>();
 
     @property()
     comment = '';
 
+    @property({ type: Boolean })
+    emotion = true;
+
     render() {
         return html`
             <div class="root">
+                <label><b>Emotion:</b></label>
+                <div class="emotion">
+                    <div class="radio">
+                        <input
+                            ${ref(this.emotionRef)}
+                            type="radio"
+                            id="true"
+                            name="emotion"
+                            value="true"
+                            ?checked=${this.emotion === true}
+                        />
+                        <label for="true">
+                            <img src="./assets/emotions/positive.jpg" />
+                        </label>
+                    </div>
+                    <div class="radio">
+                        <input
+                            type="radio"
+                            id="false"
+                            name="emotion"
+                            value="false"
+                            ?checked=${this.emotion === false}
+                        />
+                        <label for="false">
+                            <img src="./assets/emotions/negative.jpg" />
+                        </label>
+                    </div>
+                </div>
                 <label><b>Comment:</b></label>
                 <textarea
                     ${ref(this.commentRef)}
@@ -35,7 +89,8 @@ export class ItemContent extends LitElement {
 
     content() {
         return {
-            comment: this.commentRef.value?.value
+            comment: this.commentRef.value?.value,
+            emotion: this.emotionRef.value?.checked
         };
     }
 }
